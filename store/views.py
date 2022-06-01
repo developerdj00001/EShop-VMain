6c4b29915de364b2ecb78539f5a09f7ea4620530
developerdj00001 <developerdj00001@outlook.com>
2022-06-01 19:20:58 -0300

    C4

--- store/views.py
+++ store/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models.product import Product
 
 
 # Create your views here.
 def index(request):
+    products = Product.get_all_products()
+    print(products)
     return render(request, 'orders/order.html')
